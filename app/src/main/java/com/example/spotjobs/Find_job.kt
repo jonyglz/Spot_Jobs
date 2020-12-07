e55defb88e94c275b402c1305bc81dfada0463d4
jonyglz <gon18025@byui.edu>
2020-12-07 09:07:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/spotjobs/Find_job.kt
@@ -0,0 +1,11 @@
+package com.example.spotjobs
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Find_job : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_job)
+    }
+}
---
+++ app/src/main/java/com/example/spotjobs/Post_job.kt
@@ -0,0 +1,11 @@
+package com.example.spotjobs
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Post_job : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_job)
+    }
+}
